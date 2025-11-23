1b293b649f930a4d99ceb21f0b475af9c66b80d5
cordoba-uam <scordobag@uamv.edu.ni>
2025-11-22 22:34:16 -0600

    Error arreglado: Para cada pestaña de cada entidad, aparecian dos en vez de una. Ahora empieza el proceso de volver a recrear las entidades.

--- proyecto_BillManager/src/main/java/ni/edu/uamv/proyecto_BillManager/modelo/Cuenta.java
+++ proyecto_BillManager/src/main/java/ni/edu/uamv/proyecto_BillManager/modelo/Cuenta.java
@@ -4,41 +4,23 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Collection;
 
+import org.hibernate.annotations.GenericGenerator;
 import org.openxava.annotations.*;
 
 @Entity
 @Getter @Setter
-@View(members =
-        "usuario, nombre, saldoInicial, saldoActual;" + // fila 1
-                "metas;" +                                      // fila 2: colección
-                "transacciones"                                // fila 3: colección
-)
-@Tab(properties = "usuario.nombre, nombre, saldoActual")
 public class Cuenta {
 
+    private enum tipo {ACTIVO, PASIVO, PATRIMONIO}
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @Hidden
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String oid;
 
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    private Usuario usuario;
-
-    @Required
-    @Column(length = 40)
+    @Column(name = "nombre_cuenta", length = 60, nullable = false)
+    @Required(message = "La cuenta tiene que tener un nombre")
     private String nombre;
-
-    private double saldoInicial;
-    private double saldoActual;
-
-    // --- Relación con Meta ---
-    @OneToMany(mappedBy = "cuenta")
-    @ListProperties("nombre, montoObjetivo, montoAcumulado, estado")
-    private Collection<Metas> metas;
-
-    // --- Relación con Transaccion + gráfico ---
-    @OneToMany(mappedBy = "cuenta")
-    @ListProperties("fecha, descripcion, categoria.nombre, monto")
-    private Collection<Transaccion> transacciones;
 }
